7ae7e634dd62cf9348c590fbb28a0767bc2768cf
Ajay Esuri <esuri.ajay@girmiti.com>
2023-10-04 18:04:59 +0530

    Sliding window

---
+++ src/dsa/subarray/SlidingWindow.java
@@ -0,0 +1,41 @@
+package dsa.subarray;
+
+public class SlidingWindow {
+    public static void main(String[] args) {
+        int[] a = {3, 4, -2, 5, 3, -2, 8, 2, 1, 4};
+
+        int k = 2;
+        int i1 = calculateMaxSumInSubArray(a, k);
+        System.out.println(i1);
+    }
+
+    private static int calculateMaxSumInSubArray(int[] a, int k) {
+        int s = 0;
+        int e = k ;
+        int n = a.length;
+        int ans = Integer.MIN_VALUE;
+        int sum=0;
+        for (int i = s; i <e; i++) {
+           // int sum = 0;
+            sum = sum + a[i];
+            if (ans < sum) {
+                ans = sum;
+            }
+        }
+            s = 1;
+            e = k;
+            while (e < n) {
+                //int sum2 = 0;
+                sum = sum - a[s - 1]+a[e];
+
+                if (ans < sum) {
+                    ans = sum;
+                }
+                s++;
+                e++;
+            }
+
+
+        return ans;
+    }
+}
