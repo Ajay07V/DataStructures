5ac7e3605a04bd48ee9a3b35e86cc372566d9e71
Ajay Esuri <esuri.ajay@girmiti.com>
2023-10-11 22:08:48 +0530

    Changes in if condition order 9->6

--- src/dsa/hashing/FindPairMatchesK.java
+++ src/dsa/hashing/FindPairMatchesK.java
@@ -1,28 +1,33 @@
 package dsa.hashing;
 /*Given an Array of integers B, and a target sum A.
 Check if there exists a pair (i,j) such that Bi + Bj = A and i!=j*/
+/*>Suppose a case like this [3, 5] and A = 6
 
+>Now when i equal to 0 you will add 3 in your hashset and then you will check if 6 - 3 = 3 is present in hSet.
+
+>And this will true and you will return 1. But this will be wrong. As you considering same 3 as pair.
+
+>So, correct appraoch will be move line 9 if condition before line 6 if condition.*/
 
 import java.util.HashSet;
 
 public class FindPairMatchesK {
     public static void main(String[] args) {
-        int A[]={1,2,6,6,2};
-        int k=8;
-       boolean present= isPairPresent(A,k);
+        int[] A = { 9, 71, 45, 95, 55, 78, 85, 1, 43, 57, 16, 68, 15, 1, 0, 51};
+        int k = 9;
+        boolean present = isPairPresent(A, k);
         System.out.println(present);
     }
 
     private static boolean isPairPresent(int[] A, int k) {
-        HashSet<Integer> hSet=new HashSet<>();
-        int n=A.length;
-        for(int i=0;i<n;i++){
-            if(!hSet.contains(A[i])){
-                hSet.add(A[i]);
+        HashSet<Integer> hSet = new HashSet<>();
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            if (hSet.contains(k - A[i])) {
+                return true;
             }
-            if(hSet.contains(k-A[i])){
-               return true;
-            }
+            hSet.add(A[i]);
+
         }
         return false;
     }
