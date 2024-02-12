2bd608aee08ee950d4df8a700040607322c5f8dc
Esuri <ajay.esuri@dellteam.com>
2024-02-13 00:00:10 +0530

    brute force method to find max sub array sum with length k

--- src/dsa/subarray/SubArraySumLengthK.java
+++ src/dsa/subarray/SubArraySumLengthK.java
@@ -1,6 +1,6 @@
 package dsa.subarray;
 //BruteForce method
-public class SubArraySumK {
+public class SubArraySumLengthK {
     public static void main(String[] args) {
         int[] a = {2, 4, 5, 6, 2, 1};
         int k = 3;
